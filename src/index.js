9690f594abb51e12f5df61f74985b0da616592ca
Arun Kumar <arun.ce.23@nitap.ac.in>
2026-05-29 23:49:16 +0530

    Feat: Added basic express server setup with dotenv and nodemon

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const bodyParser = require("body-parser");
+
+const { PORT } = require("./config/server.config");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+
+app.get('/ping', (req, res) => {
+    return res.json({message : "problem service has arrived"});
+});
+
+app.listen(PORT, () => {
+    console.log(`server has started at port ${PORT}`);
+});
+
+
