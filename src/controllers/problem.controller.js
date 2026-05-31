214966ca0129b49ba535aaaf8553a14d45b09534
Arun Kumar <arun.ce.23@nitap.ac.in>
2026-05-31 11:38:33 +0530

    Feat: added unimplemented error for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -3,23 +3,33 @@ function pingController(req, res) {
 };
 
 function addProblem(req, res){
-
+    return res.status(501).json({
+        message: "not implemented"
+    });
 };
 
 function getProblem(req, res){
-
+    return res.status(501).json({
+        message: "not implemented"
+    });
 };
 
 function getProblems(req, res){
-
+    return res.status(501).json({
+        message: "not implemented"
+    });
 };
 
 function updateProblem(req, res){
-
+    return res.status(501).json({
+        message: "not implemented"
+    });
 };
 
 function deleteProblem(req, res){
-    
+    return res.status(501).json({
+        message: "not implemented"
+    });
 };
 
 module.exports = {
